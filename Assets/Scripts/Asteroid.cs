ae2051d046bd7d1b92a3ce342f2c60b099269116
Steve Losh <steve@stevelosh.com>
2017-02-05 19:31:24 +0000

    Add explosions

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Asteroid : MonoBehaviour {
+    public GameObject explosionPrefab;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start () {
+        StartCoroutine(DebugTest());
+    }
+
+    void Update () {
+    }
+
+    private IEnumerator DebugTest() {
+        yield return new WaitForSeconds(4.0f);
+        Explode();
+    }
+
+    // Destruction ------------------------------------------------------------
+    private void SpawnChildren() {
+        // TODO
+    }
+
+    private void SpawnExplosion() {
+        GameObject explosion = Instantiate(explosionPrefab);
+        explosion.transform.position = this.transform.position;
+        Destroy(explosion, explosion.GetComponent<ParticleSystem>().duration);
+    }
+
+    private void Explode() {
+        SpawnExplosion();
+        SpawnChildren();
+        Destroy(gameObject);
+    }
 }
