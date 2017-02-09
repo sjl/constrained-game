583fc2ec157dd49e976c90b2b8fcc0d33b4d6e55
Steve Losh <steve@stevelosh.com>
2017-02-09 11:40:51 +0000

    Add timer

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+    public Text timer;
+
+    private float clock;
+
+    void Start () {
+        clock = 120.0f;
+    }
+
+    void Update () {
+        clock -= Time.deltaTime;
+
+        if (clock < 0.0f) {
+            // TODO: Game Over
+            return;
+        }
+
+        int intClock = (int)clock;
+
+        int minutes = intClock / 60;
+        int seconds = intClock % 60;
+
+        timer.text = System.String.Format("{0:d}:{1:d}", minutes, seconds);
+    }
+}
